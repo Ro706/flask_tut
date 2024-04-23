22841d23ad3e59bad68a8805cf813d5c7879185d
Ro706 <rohitmandal99091@gmail.com>
2024-04-23 12:04:31 +0530

    update

--- tut1/app.py
+++ tut1/app.py
@@ -7,11 +7,12 @@ def index():
             <h1>Hello World</h1>
             <button><a href="/hello">Click here</a></button>
             '''
-@app.route('/hello')
+# @app.route('/hello')
 def hello():
     return '''Hey there 
             <button><a href="/">Go back</a></button>
             '''
-
+    # app.add_url_rule('/', 'helloworld', hello_world)
 if __name__=='__main__':
     app.run(debug=True)
+    app.add_url_rule('/', 'hello', hello)
